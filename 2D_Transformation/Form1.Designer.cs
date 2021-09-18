3895bd239600e9a5318dc0b4fe33580cd20d2e8a
peterezzat <55260283+peterezz@users.noreply.github.com>
2021-09-18 14:55:11 +0200

    Project Files

---
+++ 2D_Transformation/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿
+namespace _2D_Transformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trans = new System.Windows.Forms.Panel();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label29 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rotat = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label30 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.trans.SuspendLayout();
+            this.rotat.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(1, 107);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(861, 454);
+            this.panel1.TabIndex = 0;
+            // 
+            // trans
+            // 
+            this.trans.Controls.Add(this.label23);
+            this.trans.Controls.Add(this.label7);
+            this.trans.Controls.Add(this.label24);
+            this.trans.Controls.Add(this.textBox2);
+            this.trans.Controls.Add(this.textBox1);
+            this.trans.Controls.Add(this.textBox3);
+            this.trans.Controls.Add(this.label29);
+            this.trans.Controls.Add(this.label3);
+            this.trans.Controls.Add(this.label2);
+            this.trans.Location = new System.Drawing.Point(1, 45);
+            this.trans.Name = "trans";
+            this.trans.Size = new System.Drawing.Size(304, 61);
+            this.trans.TabIndex = 7;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label23.Location = new System.Drawing.Point(217, 1);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(62, 19);
+            this.label23.TabIndex = 14;
+            this.label23.Text = "Point2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(10, 1);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 19);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Point1";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label24.Location = new System.Drawing.Point(116, 1);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(0, 19);
+            this.label24.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 23);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label29
+            // 
+            this.label29.AutoSize = true;
+            this.label29.Location = new System.Drawing.Point(211, 27);
+            this.label29.Name = "label29";
+            this.label29.Size = new System.Drawing.Size(20, 15);
+            this.label29.TabIndex = 2;
+            this.label29.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Y1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X1";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(1, 34);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 1);
+            this.panel2.TabIndex = 11;
+            // 
+            // rotat
+            // 
+            this.rotat.Controls.Add(this.panel4);
+            this.rotat.Controls.Add(this.label9);
+            this.rotat.Controls.Add(this.textBox4);
+            this.rotat.Controls.Add(this.textBox6);
+            this.rotat.Controls.Add(this.textBox5);
+            this.rotat.Controls.Add(this.label30);
+            this.rotat.Controls.Add(this.label6);
+            this.rotat.Controls.Add(this.label5);
+            this.rotat.Location = new System.Drawing.Point(307, 45);
+            this.rotat.Name = "rotat";
+            this.rotat.Size = new System.Drawing.Size(308, 61);
+            this.rotat.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(91, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1, 60);
+            this.panel4.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(109, 5);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 19);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Point3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(33, 28);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 23);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(221, 24);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(52, 23);
+            this.textBox6.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(135, 24);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(52, 23);
+            this.textBox5.TabIndex = 18;
+            // 
+            // label30
+            // 
+            this.label30.AutoSize = true;
+            this.label30.Location = new System.Drawing.Point(7, 31);
+            this.label30.Name = "label30";
+            this.label30.Size = new System.Drawing.Size(20, 15);
+            this.label30.TabIndex = 1;
+            this.label30.Text = "Y2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(195, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Y3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(109, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "X3";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Translate",
+            "Scale",
+            "Rotate",
+            "Reflect",
+            "Shear"});
+            this.comboBox1.Location = new System.Drawing.Point(1, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 23);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(660, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Show Result";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(204, 45);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1, 60);
+            this.panel3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 561);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.rotat);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.trans);
+            this.Name = "Form1";
+            this.Text = "2D_Transformation";
+            this.trans.ResumeLayout(false);
+            this.trans.PerformLayout();
+            this.rotat.ResumeLayout(false);
+            this.rotat.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel trans;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel rotat;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label29;
+        private System.Windows.Forms.Label label30;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ 2D_Transformation/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2D_Transformation
+{
+    public partial class Form1 : Form
+
+    {
+        int[] point1 = { 100, 200 };
+        int[] point2 = { 200, 300 };
+        int[] point3 = { 100, 400 };
+        Pen p = new Pen(Brushes.Black, 5);
+        Graphics x;
+        Point p1, p2, p3;
+        public Form1()
+        {
+            InitializeComponent();
+            x=panel1.CreateGraphics();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        public void drawTriangle(int[] point1, int[] point2, int[] point3)
+        {
+            p1 = new Point(point1[0], point1[1]);
+            p2 = new Point(point2[0], point2[1]);
+            p3 = new Point(point3[0], point3[1]);
+            x.DrawLine(p, p1, p2);
+            x.DrawLine(p, p1, p3);
+            x.DrawLine(p, p2, p3);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == 1)
+            {
+                trans.Visible = true;
+                label24.Text = "";
+                panel3.Visible = true;
+                rotat.Visible = true;
+                label23.Text = "Point2";
+                label2.Text = "X1";
+                label3.Text = "Y1";
+                label29.Text = "X2";
+                label30.Text = "Y2";
+                label5.Text = "X3";
+                label6.Text = "Y3";
+
+            }
+             else if (comboBox1.SelectedIndex==2 || comboBox1.SelectedIndex == 4)
+            {
+                trans.Visible = true;
+                label24.Text = "Point2";
+                label23.Text = "Point3";
+                panel3.Visible = false;
+                rotat.Visible = false;
+                if (comboBox1.SelectedIndex==2)
+                {
+                    label2.Text = "θ1";
+                    label3.Text = "θ2";
+                    label29.Text = "θ3";
+                }
+                else if (comboBox1.SelectedIndex==4)
+                {
+                    label2.Text = "X1";
+                    label3.Text = "X2";
+                    label29.Text = "X3";
+                }
+                
+            }
+           
+            else if(comboBox1.SelectedIndex==3)
+            {
+                trans.Visible = false;
+                rotat.Visible = false;
+                panel3.Visible = false;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawTriangle(point1, point2, point3);
+            if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == 1)
+            {
+                if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals("") || textBox4.Text.Equals("") || textBox5.Text.Equals("") || textBox6.Text.Equals(""))
+                {
+                    string message = "field(s) are EMPTY";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+                else if (!isNum(textBox1.Text) || !isNum(textBox2.Text) || !isNum(textBox3.Text) || !isNum(textBox4.Text) || !isNum(textBox5.Text) || !isNum(textBox6.Text))
+                {
+                    string message = "enter INTEGER number to the fields";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+
+                else
+                {
+
+                    int[] transform1 = { Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text) };
+                    int[] transform2 = { Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text) };
+
+                    int[] transform3 = { Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text) };
+                    if (transform1[0] < 0 || transform1[1] < 0 || transform2[0] < 0 || transform2[1] < 0 || transform3[0] < 0 || transform3[1] < 0)
+                    {
+                        string message = "enter POSITIVE number to the fields";
+                        string title = "Error";
+                        MessageBox.Show(message, title);
+                    }
+                    else
+                    {
+                        if (comboBox1.SelectedIndex == 0)
+                        {
+                            this.point1 = Transform_2D(point1, transform1, 1.0, "Translate");
+                            this.point2 = Transform_2D(point2, transform2, 1.0, "Translate");
+                            this.point3 = Transform_2D(point3, transform3, 1.0, "Translate");
+
+                        }
+                        else if (comboBox1.SelectedIndex == 1)
+                        {
+                            this.point1 = Transform_2D(point1, transform1, 1.0, "Scale");
+                            this.point2 = Transform_2D(point2, transform2, 1.0, "Scale");
+                            this.point3 = Transform_2D(point3, transform3, 1.0, "Scale");
+                        }
+                        x.Clear(Color.White);
+                        drawTriangle(point1, point2, point3);
+                    }
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals(""))
+                {
+                    string message = "field(s) are EMPTY";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+                else if (!isDouble(textBox1.Text) || !isDouble(textBox2.Text) || !isDouble(textBox3.Text))
+                {
+                    string message = "enter DOUBLE number to the fields";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+
+                else
+                {
+                    double R1 = Convert.ToDouble(textBox1.Text);
+                    double R2 = Convert.ToDouble(textBox2.Text);
+                    double R3 = Convert.ToDouble(textBox3.Text);
+                    if (R1 < 0 || R2 < 0 || R3 < 0)
+                    {
+                        string message = "enter POSITIVE number to the fields";
+                        string title = "Error";
+                        MessageBox.Show(message, title);
+                    }
+                    else
+                    {
+                        int[] nulll = { 0 };
+                        this.point1 = Transform_2D(point1, nulll, R1, "Rotate");
+                        this.point2 = Transform_2D(point2, nulll, R2, "Rotate");
+                        this.point3 = Transform_2D(point3, nulll, R3, "Rotate");
+                        x.Clear(Color.White);
+                        drawTriangle(point1, point2, point3);
+                    }
+                }
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                int[] nulll = { 0 };
+                this.point1 = Transform_2D(point1, nulll, 1.0, "Reflect");
+                this.point2 = Transform_2D(point2, nulll, 1.0, "Reflect");
+                this.point3 = Transform_2D(point3, nulll, 1.0, "Reflect");
+                x.Clear(Color.White);
+                drawTriangle(point1, point2, point3);
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                if (textBox1.Text.Equals("") || textBox2.Text.Equals("") || textBox3.Text.Equals(""))
+                {
+                    string message = "field(s) are EMPTY";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+                else if (!isNum(textBox1.Text) || !isNum(textBox2.Text) || !isNum(textBox3.Text))
+                {
+                    string message = "enter INTEGER number to the fields";
+                    string title = "Error";
+                    MessageBox.Show(message, title);
+                }
+
+                else
+                {
+                    int[] shear1 = { Convert.ToInt32(textBox1.Text) };
+                    int[] shear2 = { Convert.ToInt32(textBox2.Text) };
+                    int[] shear3 = { Convert.ToInt32(textBox3.Text) };
+                    if (shear1[0] < 0 || shear2[0] < 0 || shear3[0] < 0)
+                    {
+                        string message = "enter POSITIVE number to the fields";
+                        string title = "Error";
+                        MessageBox.Show(message, title);
+                    }
+                    else
+                    {
+                        
+                        this.point1 = Transform_2D(point1, shear1, 1.0, "Shear");
+                        this.point2 = Transform_2D(point2, shear2, 1.0, "Shear");
+                        this.point3 = Transform_2D(point3, shear3, 1.0, "Shear");
+                        x.Clear(Color.White);
+                        drawTriangle(point1, point2, point3);
+                    }
+
+                }
+            }
+            string message1 = "Point1 : X1= "+point1[0]+", Y1= "+point1[1]+"\n Point2 : X2= "+point2[0]+", Y2= "+point2[1]+"\n Point3 : X3= "+point3[0]+", Y3= "+point3[1]+" ." ;
+            string title1 = "Message";
+            MessageBox.Show(message1, title1);
+
+        }
+
+        public Boolean isNum(string x)
+        {
+            int y;
+            try
+            {
+                y = Convert.ToInt32(x);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+
+        }
+        public Boolean isDouble(string x)
+        {
+            double y;
+            try
+            {
+                y = Convert.ToDouble(x);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+
+        }
+        public static int[] Transform_2D(int[] p, int[] t, double Thete,string type)
+        {
+            if(type.Equals("Translate"))
+            {
+                int P_dash_x = p[0] + t[0];
+                int P_dash_y = p[1] + t[1];
+                int[] p_dash = { P_dash_x, P_dash_y };
+                return p_dash;
+
+            }
+            else if(type.Equals("Scale"))
+            {
+                int P_dash_x = p[0] * t[0];
+                int P_dash_y = p[1] * t[1];
+                int[] p_dash = { P_dash_x, P_dash_y };
+                return p_dash;
+
+            }
+            else if (type.Equals("Rotate"))
+            {
+                int P_dash_x = (p[0] * Convert.ToInt32(Math.Cos(Thete))) - (p[1] * Convert.ToInt32(Math.Sin(Thete)));
+                int P_dash_y = (p[0] * Convert.ToInt32(Math.Sin(Thete))) + (p[1] * Convert.ToInt32(Math.Cos(Thete)));
+                int[] p_dash = { P_dash_x, P_dash_y };
+                return p_dash;
+
+            }
+            else if(type.Equals("Reflect"))
+            {
+                int P_dash_x = p[0] * -1;
+                int P_dash_y = p[1] * -1;
+                int[] p_dash = { P_dash_x, P_dash_y };
+                return p_dash;
+
+            }
+            else if(type.Equals("Shear"))
+            {
+                int P_dash_x = p[0] + (t[0]*p[1]);
+                int P_dash_y = p[1] ;
+                int[] p_dash = { P_dash_x, P_dash_y };
+                return p_dash;
+            }
+            int[] error = { 0 };
+            return error;
+
+            
+        }
+      
+    }
+}
